74a18889b988cb376f4571ab4b5099fc1f5af523
gabtoledano <gabrieltoledano19@gmail.com>
2025-09-22 17:28:21 +0200

    handleSumbmit halfway done

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,23 @@
+const loginApi = "http://localhost:5678/api/users/login";
+
+document.getElementById("loginform").addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  let user = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+
+  let response = await fetch(loginApi, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  let result = await response.json();
+  console.log(result);
+}
